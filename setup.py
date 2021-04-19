4f6da6fbf08abb4026324535cf9a5113ae20116a
nik <nik@heartex.net>
2021-04-19 22:05:05 +0300

    Update to version 0.0.15

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='label-studio-evalme',
-    version='0.0.14',
+    version='0.0.15',
     author='Heartex',
     author_email="hello@heartex.ai",
     description='Evaluation metrics',
