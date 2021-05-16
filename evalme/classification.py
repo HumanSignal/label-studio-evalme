f690b6a89f978fc1e0d0afba5c951ed43bf2ee3e
nik <nik@heartex.net>
2021-05-16 11:17:48 +0300

    Support shape keys and non list values for choices

--- evalme/classification.py
+++ evalme/classification.py
@@ -22,7 +22,7 @@ class ClassificationEvalItem(EvalItem):
 
             # choices are mismatched
             if x[self._shape_key] != y[self._shape_key]:
-                if per_label:
+                if per_label and isinstance(x[self._shape_key], list):
                     for l in x[self._shape_key]:
                         total_weight[l] = 0
                 else:
@@ -30,15 +30,20 @@ class ClassificationEvalItem(EvalItem):
 
             # choices are matched
             else:
-                if per_label:
+                if per_label and isinstance(x[self._shape_key], list):
                     # per label mode: label weights are unimportant
                     for l in x[self._shape_key]:
                         total_weight[l] = 1
                 else:
                     # aggregation mode: average scores by label weights
-                    weight = sum(label_weights.get(l, 1) for l in x[self._shape_key])
-                    total_weight += weight
-                    n += len(x[self._shape_key])
+                    if isinstance(x[self._shape_key], list):
+                        weight = sum(label_weights.get(l, 1) for l in x[self._shape_key])
+                        total_weight += weight
+                        n += len(x[self._shape_key])
+                    else:
+                        weight = 1
+                        total_weight += weight
+                        n += 1
         if per_label:
             return total_weight
         if n == 0:
@@ -54,21 +59,21 @@ class PairwiseEvalItem(ClassificationEvalItem):
     SHAPE_KEY = 'pairwise'
 
 
-def _as_choices(item):
+def _as_choices(item, shape_key):
     if not isinstance(item, ChoicesEvalItem):
-        return ChoicesEvalItem(item)
+        return ChoicesEvalItem(item, shape_key=shape_key)
     return item
 
 
-def _as_pairwise(item):
+def _as_pairwise(item, shape_key):
     if not isinstance(item, PairwiseEvalItem):
-        return PairwiseEvalItem(item)
+        return PairwiseEvalItem(item, shape_key=shape_key)
     return item
 
 
-def exact_matching_choices(item_gt, item_pred, label_weights=None, per_label=False):
-    return _as_choices(item_gt).exact_match(_as_choices(item_pred), label_weights, per_label=per_label)
+def exact_matching_choices(item_gt, item_pred, label_weights=None, per_label=False, shape_key=None):
+    return _as_choices(item_gt, shape_key).exact_match(_as_choices(item_pred, shape_key), label_weights, per_label=per_label)
 
 
-def exact_matching_pairwise(item_gt, item_pred, label_weights=None, per_label=False):
-    return _as_pairwise(item_gt).exact_match(_as_pairwise(item_pred), label_weights, per_label=per_label)
+def exact_matching_pairwise(item_gt, item_pred, label_weights=None, per_label=False, shape_key=None):
+    return _as_pairwise(item_gt, shape_key).exact_match(_as_pairwise(item_pred, shape_key), label_weights, per_label=per_label)
