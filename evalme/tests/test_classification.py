d2c6dc05de6f05117ed023bfca5c5ea3ab251bc7
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2022-04-06 19:04:22 +0300

    Add test and per label fix

--- evalme/tests/test_classification.py
+++ evalme/tests/test_classification.py
@@ -4,6 +4,8 @@ from evalme.classification import ClassificationEvalItem, ChoicesEvalItem, naive
 
 from evalme.metrics import Metrics
 
+from classification import exact_matching_choices
+
 
 @pytest.mark.ClassificationEvalItem
 def test_not_matching():
@@ -284,3 +286,17 @@ def test_naive_not_matching_per_label():
         ]]
     assert naive(test_data[0], test_data[1], per_label=True) == {"Accessories1": 0}
     assert Metrics.apply({}, test_data[0], test_data[1], metric_name='naive', per_label=True) == {"Accessories1": 0, "Accessories2": 0}
+
+
+def test_dynamic_choices():
+    test_data1 = [{'value': {'choices': [['Products', 'Loan Payment Center']]}, 'id': 'edeDdGdNnb',
+                   'from_name': 'dynamic_choices', 'to_name': 'text', 'type': 'choices', 'origin': 'manual'}]
+    score = exact_matching_choices(test_data1,
+                                   test_data1,
+                                   {})
+    score_per_label = exact_matching_choices(test_data1,
+                                             test_data1,
+                                             {},
+                                             per_label=True)
+    assert score == 1
+    assert score_per_label == {'Products': 1, 'Loan Payment Center': 1}
