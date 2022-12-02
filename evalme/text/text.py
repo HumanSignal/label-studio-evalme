ada233612779c9e289886fae7e1f34eb748f2658
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2022-12-02 15:11:10 +0300

    Update match to check condition in case no region detected

--- evalme/text/text.py
+++ evalme/text/text.py
@@ -428,7 +428,9 @@ class SimpleComparisionEvalItem(EvalItem):
             return int(gt[self._shape_key] == pred[self._shape_key]), \
                    EvalItem.general_iou_by_type(region, gt, pred)
         logger.debug(f"Returning simple calculation")
-        return int(gt[self._shape_key] == pred[self._shape_key]), 1.0
+        if check_condition:
+            return int(gt[self._shape_key] == pred[self._shape_key]), 1.0
+        return int(gt[self._shape_key] == pred[self._shape_key])
 
     def match(self, pred, per_label=False, **kwargs):
         if per_label:
